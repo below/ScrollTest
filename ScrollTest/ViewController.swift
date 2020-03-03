21abcd6f7adf95835096725fa2f121d52249ca65
Alexander von Below <alexander.von_below@telekom.de>
2020-03-03 14:30:24 +0100

    Unworking Scroll View

--- ScrollTest/ViewController.swift
+++ ScrollTest/ViewController.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2020 Alexander von Below. All rights reserved.
 //
 
+
+// Ruler Figure Licensed under CC 4.0 from https://commons.wikimedia.org/wiki/File:Ruler_Figure.png
+
 import UIKit
 
 class ViewController: UIViewController {
